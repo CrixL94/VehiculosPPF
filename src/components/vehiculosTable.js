46a4edd8b9291e76c02e64d283fe14d44cf03a7e
CrixL94 <lopezvasquezcristhian@gmail.com>
2023-10-27 11:07:35 -0600

    feat(tabla vehiculos)
    obteniendo datos desde el API

--- src/components/vehiculosTable.js
+++ src/components/vehiculosTable.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import axios from 'axios'; // Importa axios para realizar la solicitud
 
-function Vehiculos() {
-  return (
-    <div>
-      <h2>Vehículos</h2>
-      <p>Este es el componente de Vehículos.</p>
-    </div>
-  );
+export default function Vehiculos() {
+    const [vehiculos, setVehiculos] = useState([]);
+
+    useEffect(() => {
+        // Realiza una solicitud GET a tu ruta de API para obtener la lista de vehículos
+        axios.get('http://localhost:9000/api/get-vehiculos')
+            .then((response) => {
+                setVehiculos(response.data);
+            })
+            .catch((error) => {
+                console.error('Error al obtener la lista de vehículos:', error);
+            });
+    }, []);
+
+    return (
+        <div className="card">
+            <DataTable value={vehiculos} tableStyle={{ minWidth: '50rem' }}>
+                <Column field="marca" header="Marca"></Column>
+                <Column field="modelo" header="Modelo"></Column>
+                <Column field="placa" header="Placa"></Column>
+            </DataTable>
+        </div>
+    );
 }
 
-export default Vehiculos;
